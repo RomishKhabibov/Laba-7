e6e861a53c80d38702fbd95e3d842279975a9c28
Romish Khabibov <55175701+RomishKhabibov@users.noreply.github.com>
2020-06-17 16:16:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,40 @@
 <?php
-	if ($_POST['submit'])
-	{
-		$a = (int)$_POST['first'];
-		$b = (int)$_POST['second'];
-		$p = $_POST ['operation'];
-			if($p=='+')
-				$c = $a + $b;
-			else if($p=='-')
-				$c = $a - $b;
-			else if($p=='*')
-				$c = $a * $b;
-			else if($p=='/')
-			{
-				if ($b==0)
-					echo "You cannot divide by zero;"
-				else $c = $a / $b;
-			}
-			else $c = pow($a, $b);
-			
-		}
+function clearData($data, $type='i'){
+	switch($type){
+		case 'i': return $data*1; break;
+		case 's': return trim(strip_tags($data)); break;
+	}
+}
+$output ="";
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+	$number1 = clearData($_POST['number1']);
+	$number2 = clearData($_POST['number2']);
+	$operator = clearData($_POST['operator']), 's');
+	switch($operator){
+		case '+': $output .= $number1 + $number2; break;
+		case '-': $output .= $number1 - $number2; break;
+		case '*': $output .= $number1 * $number2; break;
+		case '/':
+			if($number2 ==0)
+				$output = "You cannot divide by zero!";
+			else
+				$output .= $number1 / $number2;
+			break;
+			default: $output = "Unknown operator '$operator'";
+	}
+}
 ?>
-<form action="" method="post">
-	<input type="textbox" name="first" required>
-	
-	<select name="operation" required>
-	<option value="+">+</option>
-	<option value="-">-</option>
-	<option value="*">*</option>
-	<option value="/">/</option>
-	<option value="^">^</option>
-	</select>
-	
-	<input type="textbox" name="second" required>
-	
-	<input type="submit" name="submit">
+<form action="<?=$_SERVER ['PHP_SELF']?>" method="post">
+Number 1:<br />
+<input type="text" name="number1"><br />
+Operator:<br />
+<input type="text" name="operator"><br />
+Number 2:<br />
+<input type="text" name="number2"><br />
+<input type="submit" value="submit">
 </form>
 <?php
-		echo $c;
+if($output){
+	echo $output;
+}
 ?>
- 
